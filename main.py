92dc11282519a00027ee75ebe9892db9bdde4f43
aL0NEW0LF <mohamedfakhreeddine2019@gmail.com>
2023-12-02 15:11:24 +0100

    feat: B+ tree

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from bplustree import BPlusTree
 from hashTableSeparateChainingWithLinkedList import HashTable
 from hashTableSeparateChainingWithList import HashTableSC
 from hashTableDoubleHashing import HashTableDH
+import random  # for demo test
 
 if __name__ == '__main__':
     ht = HashTableDH()
@@ -10,3 +12,16 @@ if __name__ == '__main__':
     ht.insertDH("test_key", "test_value3")
 
     ht.printAll()
+
+    bplustree = BPlusTree(6)
+    random_list = random.sample(range(1, 100), 20)
+    for i in random_list:
+        bplustree[i] = 'test' + str(i)
+        print('Insert ' + str(i))
+        bplustree.show()
+
+    random.shuffle(random_list)
+    for i in random_list:
+        print('Delete ' + str(i))
+        bplustree.delete(i)
+        bplustree.show()
